0f2042f5072c153a6a7d40029cb48f1e85fac37b
mingthai <mingthai@gmail.com>
2021-03-04 21:03:45 +0700

    04_03_21 Install Bootstrap

--- mingthai/react-website/src/App.js
+++ mingthai/react-website/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <div class="row">
+      <div class="col-sm">Baris 1 Kolom 1</div>
+      <div class="col-sm">Baris 1 Kolom 2</div>
+      <div class="col-sm">Baris 1 Kolom 2</div>
+
     </div>
+    <div class="row">
+      <div class="col-sm">Baris 2 Kolom 1</div>
+      <div class="col-sm">Baris 2 Kolom 2</div>
+      <div class="col-sm">Baris 2 Kolom 3</div>
+    </div>
+  </div>
   );
 }
 

6a815147b20f7a1d68cfc3d291b08743f6027e4d
Ray <monsherr@hotmail.com>
2021-06-19 19:02:26 +0700

    add few notes

--- Ray/bbstdclass-app/src/App.js
+++ Ray/bbstdclass-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Testing from './component/helloworld';
 import React, { useState } from 'react';
@@ -13,11 +12,11 @@ function App() {
 	const [a, seta] = useState('hai aku string');
 	return (
 		<div>
-			{test}
-			<A />
-			<Testing />
-      <input type="text" name="" id="" onChange={(e)=>seta(e.target.value)} />
-			{a}
+			{test} {/* diambil dari const line 5*/}
+			<A /> {/* diambil dari const line 7*/}
+			<Testing />{/* diambil import line 2*/}
+      <input type="text" name="" id="" onChange={(e)=>seta(e.target.value)} /> {/* diambil dari const line 12*/}
+			{a} {/* diambil dari const line 12*/}
 		</div>
 	);
 }

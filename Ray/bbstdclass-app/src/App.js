33a2b584738c05e8d2857e645f0ca28dcb58f1f6
Ray <monsherr@hotmail.com>
2021-06-21 16:57:59 +0700

    Upd

--- Ray/bbstdclass-app/src/App.js
+++ Ray/bbstdclass-app/src/App.js
@@ -1,26 +1,109 @@
-import './App.css';
-import Testing from './component/helloworld';
-import React, { useState } from 'react';
+// import './App.css';
+// import Testing from './component/helloworld';
+// import React, { useState } from 'react';
 
-const test = 'test';
+// const test = 'test';
 
-const A = () => {
-	return <h3>hallo!</h3>;
-};
+// const A = () => {
+// 	return <h3>hallo!</h3>;
+// };
 
-function App() {
-	const [a, setA] = useState('hai aku string');
-	return (
-		<div>
-			{test} {/* diambil dari const line 5*/}
-			<A /> {/* diambil dari const line 7*/}
-			<Testing />
-			{/* diambil import line 2*/}
-			<input type="text" onChange={(e) => setA(e.target.value)} />
-			{/* diambil dari const line 12*/}
-			{a} {/* diambil dari const line 12*/}
-		</div>
-	);
+// function App() {
+// 	const [a, setA] = useState('hai aku string');
+// 	return (
+// 		<div>
+// 			{test} {/* diambil dari const line 5*/}
+// 			<A /> {/* diambil dari const line 7*/}
+// 			<Testing />
+// 			{/* diambil import line 2*/}
+// 			<input type="text" onChange={(e) => setA(e.target.value)} />
+// 			{/* diambil dari const line 12*/}
+// 			{a} {/* diambil dari const line 12*/}
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+// import './App.css';
+// import React, { Component } from 'react';
+
+// export default class App extends Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			nama: 'babastudio',
+// 		};
+// 	}
+
+// 	handleNama = (event) => {
+// 		this.setState({ nama: event.target.value });
+//     console.log(this.setState({ nama: event.target.value }));
+// 	};
+
+// 	updateState = () => {
+// 		this.setState({ nama: 'murid babastudio' });
+// 	};
+
+// 	handleSubmit = (event) => {
+// 		alert(`nama yang tersimpan : ${this.state.nama}`);
+// 		event.preventDefault();
+// 	};
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<form onSubmit={this.handleSubmit}>
+// 					<input
+// 						type="text"
+// 						value={this.state.nama}
+// 						onChange={this.handleNama}
+// 						placeholder="masukkan nama"
+// 					/>
+// 					<input type="submit" value="Submit" />
+// 				</form>
+// 			</div>
+// 		);
+// 	}
+// }
+
+import React, { Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	NavLink,
+} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Links = () => (
+	<div className="list-group">
+		<NavLink className="list-group-item" to="/">
+			Home
+		</NavLink>
+
+		<NavLink className="list-group-item" to="/about">
+			About
+		</NavLink>
+	</div>
+);
+export default class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="row">
+					<section className="col-sm-4">
+						<Links />
+					</section>
+					<section className="col-sm-8">
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+					</section>
+				</div>
+			</Router>
+		);
+	}
 }
-
-export default App;
